54cba5da08f195c774ee42f61974408f21e84074
amanmdesai <amanmukeshdesai@gmail.com>
2022-07-10 13:55:55 +0530

    add download samples

---
+++ download.sh
@@ -0,0 +1,10 @@
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/DYJetsToLL.root
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/VBF_HToTauTau.root
+
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/TTbar.root
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/W1JetsToLNu.root
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/W2JetsToLNu.root
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/W3JetsToLNu.root
+
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/Run2012B_TauPlusX.root
+xrdcp root://eospublic.cern.ch//eos/root-eos/HiggsTauTauReduced/Run2012C_TauPlusX.root
